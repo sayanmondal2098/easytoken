4d4e2cca56472baa6da09c3e6f368b263e0f6323
Sayan Mondal <sayanmondal2098@gmail.com>
2019-12-29 01:53:27 +0530

    update setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+setup(
+  name = 'tokenizer',         
+  packages = ['tokenizer'],   
+  version = '1.0.1',      
+  license='MIT', 
+  long_description = 'Tokenizer is an independent Open Source, Natural Language Processing python library which implements a tokenizer to create token from Both Sentence and Paragraph.',
+  description = 'Tokenizer is an independent Open Source, Natural Language Processing python library which implements a tokenizer to create token from Both Sentence and Paragraph.',  
+  author = 'Sayan MOndal(ph3n1x)',               
+  author_email = 'sayanmondal2098@gmail.com',     
+  url = 'https://github.com/sayanmondal2098/tokenizer',   
+  download_url = 'https://github.com/sayanmondal2098/tokenizer',   
+  keywords = ['NLP', 'Natural Language Processing', 'Tokenization', 'Text Summarization', 'Text Processing', 'Sentiment Analysis'],   # Keywords that define your package best
+  install_requires=[         
+          'nltk',
+          're',
+          'sys',
+          'os',
+          'itertools',
+          'string',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',     
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
