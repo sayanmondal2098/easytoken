627e5af92241cb50f371ad0f74e73c7f59382cd5
PH3N1X <mondalsayan556@gmail.com>
2019-12-30 01:16:00 +0530

    Update test.py
    update

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 import easytoken.easytoken
 from easytoken.easytoken import Wordeasytoken
 
-print(Wordeasytoken.tokenize(Wordeasytoken,text="Hello all , I'll fuck you."))
+print(Wordeasytoken.tokenize(Wordeasytoken,text="Hello all , I'll be in love with you."))
