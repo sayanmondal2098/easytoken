ea0674923eb743d959e5bcdfbbc367d8b714a562
Sayan Mondal <sayanmondal2098@gmail.com>
2020-01-26 21:47:21 +0530

    update 2.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(
   name = 'easytoken',         
   packages = ['easytoken'],   
-  version = '2.0.1',      
+  version = '2.0.2',      
   license='MIT', 
   long_description=open('README.md').read(),
   long_description_content_type='text/markdown',
